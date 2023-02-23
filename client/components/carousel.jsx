0e17364c4fe0adcaab1c43063c2e4829031f5122
Brandon Chin <bbrandonchin@yahoo.com>
2023-02-23 22:31:43 +0000

    Added set interval to carousel

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import jordanImage from '../../images/jordan.png';
 import newBalance from '../../images/new-balance.png';
 
@@ -9,6 +9,14 @@ export function Carousel() {
     { url: newBalance, title: 'New Balance' }
   ];
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentIndex((currentIndex + 1) % images.length);
+    }, 3000);
+
+    return () => clearInterval(intervalId);
+  });
 
   function goToPrevious() {
     const isFirstSlide = currentIndex === 0;

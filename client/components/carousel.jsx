75a80904792efde83434f4518723555baa7105ca
Brandon Chin <bbrandonchin@yahoo.com>
2023-03-21 03:13:09 +0000

    adjusted container width.

--- client/components/carousel.jsx
+++ client/components/carousel.jsx
@@ -33,11 +33,11 @@ export default function Carousel({ onAnchorClick }) {
 
   return (
     <>
-      <div className='center flex'>
+      <div className='justify-center flex'>
         <div className='container'>
           <div className='flex justify-center items-center'>
             <img src={carouselImages[currentIndex].url} className='w-full h-72 xl:h-96 object-cover relative'/>
-            <i className="fas fa-chevron-left absolute left-0 md:left-24 lg:left-1/4 " onClick={goToPrevious} />
+            <i className="fas fa-chevron-left absolute left-0 md:left-24 lg:left-32 xl:left-1/4" onClick={goToPrevious} />
             <i className="fas fa-chevron-right absolute right-0 md:right-24 lg:right-1/4" onClick={goToNext} />
           </div>
         </div>

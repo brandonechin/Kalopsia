4226824e1f78ff3ad0fe5aab08cf05e2c9a844c1
Brandon Chin <bbrandonchin@yahoo.com>
2023-03-29 04:15:05 +0000

    Added get request for cart items when user accesses cart page.

--- client/pages/cart.jsx
+++ client/pages/cart.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function Cart() {
+
+  useEffect(() => {
+    async function cartItems() {
+      const cartId = 1;
+      try {
+        const response = await fetch(`/api/cart-items/${cartId}`);
+        const data = await response.json();
+        // eslint-disable-next-line no-console
+        console.log(data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    cartItems();
+  }, []);
+
   return (
     <div className='h-screen flex justify-center items-center'>
       <h1 className='text-4xl'>Under Construction...</h1>

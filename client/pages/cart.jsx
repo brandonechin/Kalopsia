57ebde26add940088fc1bd8193c526320d8588d0
Brandon Chin <bbrandonchin@yahoo.com>
2023-03-30 00:48:28 +0000

    Created skeleton for mobile view.

--- client/pages/cart.jsx
+++ client/pages/cart.jsx
@@ -37,8 +37,82 @@ export default function Cart({ products }) {
   console.log(cartProductsInfo);
 
   return (
-    <div className='h-screen flex justify-center items-center'>
-      <h1 className='text-4xl'>Under Construction...</h1>
+  // <div className='flex justify-center items-center'>
+  //   <h1 className='text-4xl'>Under Construction...</h1>
+  // </div>
+    <div className='h-screen'>
+      <div className='md:w-11/12 lg:w-10/12 xl:w-1/2'>
+        {/* <div className='flex flex-wrap'>
+          <div className='basis-1/2'>
+            hi
+          </div>
+          <div className='basis-1/2'>
+            <div className='flex'>
+              <h1>Summary</h1>
+            </div>
+            <div className='flex'>
+              <div className='flex justify-center basis-1/2'>
+                <h1>Subtotal</h1>
+              </div>
+              <div className='flex justify-center basis-1/2'>
+                <h1>$900</h1>
+              </div>
+            </div>
+            <div className='flex'>
+              <div className='flex justify-center basis-1/2'>
+                <h1>Total</h1>
+              </div>
+              <div className='flex justify-center basis-1/2'>
+                <h1>$900</h1>
+              </div>
+            </div>
+          </div>
+        </div> */}
+        <div className='flex justify-center'>
+          Bag
+        </div>
+        <div className='flex justify-center'>
+          X Items | $907
+        </div>
+        <div>
+          shoe info
+        </div>
+        <div>
+          Summary
+        </div>
+        <div className='flex justify-between'>
+          <div>
+            Subtotal
+          </div>
+          <div>
+            $500
+          </div>
+        </div>
+        <div className='flex justify-between'>
+          <div>
+            Shipping
+          </div>
+          <div>
+            $500
+          </div>
+        </div>
+        <div className='flex justify-between'>
+          <div>
+            Tax
+          </div>
+          <div>
+            $500
+          </div>
+        </div>
+        <div className='flex justify-between'>
+          <div>
+            Total
+          </div>
+          <div>
+            $500
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
